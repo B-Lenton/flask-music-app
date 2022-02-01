b6760d6bece0d2c00165557f931d035055c8329e
Ben Lenton <benlenton@gmail.com>
2022-02-01 17:37:28 +0000

    bare bones of templates created and app routes working

---
+++ app.py
@@ -0,0 +1,27 @@
+from turtle import title
+from flask import Flask, flash, render_template, request, url_for, redirect, abort
+
+app = Flask(__name__)
+
+"""
+Home route
+"""
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title="Home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/add-songs")
+def add_songs():
+    return render_template("add-songs.html", title="Add Songs")
+
+@app.route("/songs")
+def songs():
+    return render_template("songs.html", title="Songs")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8124)
